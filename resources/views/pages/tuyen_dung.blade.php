713937b580684800708b66e85a8d409b1d0ec293
hanhdoan1999 <doanhonghanh.1999@gmail.com>
2019-08-06 23:50:50 +0700

    abc

--- resources/views/pages/tuyen_dung.blade.php
+++ resources/views/pages/tuyen_dung.blade.php
@@ -5,47 +5,63 @@
 
 
 <!-- ****tuyen dung **** -->
-<div class="tp-bgimg defaultimg " data-bgcolor="undefined" style="background-repeat: no-repeat ; background-size: cover; background-position: center center; width: 100%; height: 100%; opacity: 1; visibility: inherit; z-index: 20;" >
-  <img  src="https://www.itgvietnam.com/wp-content/uploads/2018/08/gioi-thieu-2.jpg" style="width: 1720px;height: 633px">
-</div>
-<div class="gtchung_1" style="background-image:url('https://www.itgvietnam.com/wp-content/uploads/2018/12/banner-2.jpg');background-size: cover;background-position: center;background-repeat: no-repeat;" style="width: 1170px;height: 70px">
-  <div class="container">
-    <div class="row" style="    padding-top: 361px;">
-      <div class="col-md-6 col-md-offset-6">
-        <h3 class="title_td">CƠ HỘI NGHỀ NGHIỆP</h3>
-        <div class="excrepts">
-          <p>ITG mang đến cho bạn nhiều cơ hội nghề nghiệp đặc biệt trong lĩnh vực Giải pháp phần mềm quản trị doanh nghiệp – một trong những lĩnh vực hot nhất hiện nay.</p>
-          <a href="https://itgvietnam.com/category/tuyen-dung"><button class="sb_btn">Xem vị trí đang tuyển dụng</button></a>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-  <div class="gtchung_2" style="background-image:url('https://www.itgvietnam.com/wp-content/uploads/2018/06/bg2.jpg');background-size: cover;background-position: center;background-repeat: no-repeat;">
-    <div class="container">
-      <div class="row" style="padding-top: 256px;">
-        <div class="col-md-6 ">
-          <h3 class="title_td">MÔI TRƯỜNG LÀM VIỆC</h3>
-          <div class="excrepts">
-            <p>ITG luôn đặt con người làm trung tâm trong mọi hoạt động. Trở thành một nhân viên của ITG bạn sẽ luôn được tôn trọng, hỗ trợ tận tâm, giúp bạn có thể hoàn thành tốt nhất công việc của mình. Các thành viên trong công ty luôn cùng nhau chia sẻ giá trị, hỗ trợ lẫn nhau phát triển theo định hướng chung của công ty.</p>
-            <a href="https://www.itgvietnam.com/category/moi-truong-lam-viec/"><button class="sb_btn">Đi tới gallery</button></a>
+
+<main id="tuyendung">
+    <section class="bg-img">
+        <img src="https://www.itgvietnam.com/wp-content/uploads/2018/08/gioi-thieu-2.jpg" alt="">
+    </section>
+
+    <section>
+      <div class="box-content" style="background-image:url('https://www.itgvietnam.com/wp-content/uploads/2018/12/banner-2.jpg')">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-6"></div>
+              <div class="col-md-6">
+                <div class="box-text">
+                    <h6>CƠ HỘI NGHỀ NGHIỆP</h6>
+                    <p>ITG mang đến cho bạn nhiều cơ hội nghề nghiệp đặc biệt trong lĩnh vực Giải pháp phần mềm quản trị doanh nghiệp – một trong những lĩnh vực hot nhất hiện nay.Xem vị trí đang tuyển dụng</p>
+                    <button type="button" class="sb_btn">Xem vị trí đang tuyển dụng</button>
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
       </div>
-    </div>
-  </div>
-  <div class="gtchung_3" style="background-image:url('https://www.itgvietnam.com/wp-content/uploads/2018/12/tuyen-dung-01.jpg');background-size: cover;background-position: center;background-repeat: no-repeat;">
-    <div class="container">
-      <div class="row"style="    padding-top: 361px;">
-        <div class="col-md-6 col-md-offset-6">
-          <h3 class="title_td">dành cho thực tập sinh</h3>
-          <div class="excrepts">
-            <p>ITG mang đến cho bạn nhiều vị trí thực tập trong từng phòng ban, giúp bạn rèn luyện kiến thức, kĩ năng vững vàng ngay từ khi còn là sinh viên, với cơ hội trở thành nhân viên chính thức ngay sau khi kết thúc thời gian thực tập. ITG liên tục tuyển dụng thực tập sinh và trả lương theo năng lực. Hãy để lại CV ứng tuyển để gia nhập gia đình ITG.</p>
-            <a href="https://www.itgvietnam.com/?landingpage=7331"><button class="sb_btn">Apply now</button></a>
+    </section>
+
+    <section>
+      <div class="box-content" style="background-image:url('https://www.itgvietnam.com/wp-content/uploads/2018/06/bg2.jpg')">
+          <div class="container">
+            <div class="row">
+              
+              <div class="col-md-6">
+                <div class="box-text">
+                    <h6>MÔI TRƯỜNG LÀM VIỆC</h6>
+                    <p>ITG luôn đặt con người làm trung tâm trong mọi hoạt động. Trở thành một nhân viên của ITG bạn sẽ luôn được tôn trọng, hỗ trợ tận tâm, giúp bạn có thể hoàn thành tốt nhất công việc của mình. Các thành viên trong công ty luôn cùng nhau chia sẻ giá trị, hỗ trợ lẫn nhau phát triển theo định hướng chung của công ty.</p>
+                    <button type="button" class="sb_btn">Đi tới gallery</button>
+                </div>
+              </div>
+              <div class="col-md-6"></div>
+            </div>
           </div>
-        </div>
       </div>
-    </div>
-  </div>
-    
+    </section>
+
+    <section>
+      <div class="box-content" style="background-image:url('https://www.itgvietnam.com/wp-content/uploads/2018/12/tuyen-dung-01.jpg')">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-6"></div>
+              <div class="col-md-6">
+                <div class="box-text">
+                    <h6>CƠ HỘI NGHỀ NGHIỆP</h6>
+                    <p>ITG mang đến cho bạn nhiều cơ hội nghề nghiệp đặc biệt trong lĩnh vực Giải pháp phần mềm quản trị doanh nghiệp – một trong những lĩnh vực hot nhất hiện nay.Xem vị trí đang tuyển dụng</p>
+                    <button type="button" class="sb_btn">Xem vị trí đang tuyển dụng</button>
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>
+    </section>
+</main>
+
 @endsection
