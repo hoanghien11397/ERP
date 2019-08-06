f0c0c3b4e9e9b825c7e53870077a345528dcb351
NguyenThiHongTham99 <nguyenthihongtham170199@gmail.com>
2019-08-06 10:26:54 +0700

    lienhe and tuyendung

---
+++ resources/views/pages/lien_he.blade.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/lien_he.css') }}">
+<link rel="stylesheet" type="text/css" href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<script src="{{asset('')}}layout/js/jquery-1.11.1.min.js"></script>
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+    <div class="tp-bgimg defaultimg " data-bgcolor="undefined" style="background-repeat: no-repeat; background-image: url(&quot;https://www.itgvietnam.com/wp-content/uploads/2018/06/lien-he.jpg&quot;); background-size: cover; background-position: center center; width: 100%; height: 100%; opacity: 1; visibility: inherit; z-index: 20;"></div>
+<img  src="https://www.itgvietnam.com/wp-content/uploads/2018/06/lien-he.jpg" style="width: 1720px;height: 633px">
+<div class="contact_box1">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 add_col">
+                <ul class="ft_ct_list_lh">
+                    <li class="text-uppercase">CÔNG TY CỔ PHẦN GIẢI PHÁP ERP-ITG</li>
+                    <li>Văn phòng Hà Nội:</li>
+                    <li class="p_ct">Tầng 7, Tòa nhà Báo Nông thôn Ngày nay, Lô E2  Cầu Giấy, Đường Dương Đình Nghệ, Cầu Giấy, Hà Nội</li>
+                </ul>
+                <div class="g_maps">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.2956510297654!2d105.78384781485552!3d21.02085309342418!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab539595c3bd%3A0x39731e41b0eeb5d1!2zQ8O0bmcgdHkgQ1AgR2nhuqNpIFBow6FwIEVSUCAtIElURw!5e0!3m2!1svi!2s!4v1532686949267" width="550px" height="450" frameborder="0" style="border:0" allowfullscreen=""></iframe>                </div>
+                </div>
+            <div class="col-md-6">
+                <h3 class="col_title">Đăng ký tư vấn Giải pháp và nhận tài liệu miễn phí</h3>
+                <div class="ctf7_box">
+                    <div role="form" class="wpcf7" id="wpcf7-f2141-o1" lang="vi" dir="ltr">
+                        <div class="screen-reader-response"></div>
+                            <form action="/lien-he/#wpcf7-f2141-o1" method="post" class="wpcf7-form" novalidate="novalidate">
+                                <div style="display: none;">
+                                    <input type="hidden" name="_wpcf7" value="2141">
+                                    <input type="hidden" name="_wpcf7_version" value="5.1.3">
+                                    <input type="hidden" name="_wpcf7_locale" value="vi">
+                                    <input type="hidden" name="_wpcf7_unit_tag" value="wpcf7-f2141-o1">
+                                    <input type="hidden" name="_wpcf7_container_post" value="0">
+                                </div>
+                      
+                                    <p>
+                                        <span class="wpcf7-form-control-wrap your-name" style="">
+                                            <input type="text" name="your-name" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required form-control" aria-required="true" aria-invalid="false" placeholder="Họ tên" style="font-size: 14px"></span> </p>
+                                    <p>
+                                        <span class="wpcf7-form-control-wrap your-subject">
+                                            <input type="text" name="your-subject" value="" size="40" class="wpcf7-form-control wpcf7-text form-control" aria-invalid="false" placeholder="Tên doanh nghiệp tổ chức"style="font-size: 14px"></span> </p>
+                                    <p>
+                                        <span class="wpcf7-form-control-wrap your-tel">
+                                            <input type="tel" name="your-tel" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-tel wpcf7-validates-as-required wpcf7-validates-as-tel form-control" aria-required="true" aria-invalid="false" placeholder="Số điện thoại"style="font-size: 14px"></span> </p>
+                                    <p>
+                                        <span class="wpcf7-form-control-wrap your-email">
+                                            <input type="email" name="your-email" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email form-control" aria-required="true" aria-invalid="false" placeholder="Email"style="font-size: 14px"></span> </p>
+                                    <p>
+                                        <span class="wpcf7-form-control-wrap your-message">
+                                            <textarea name="your-message" cols="40" rows="10" class="wpcf7-form-control wpcf7-textarea form-control" aria-invalid="false" placeholder="Nội dung"style="font-size: 14px"></textarea></span> </p>
+                                    <p>
+                                        </p>
+                                        <div id="cf7sr-5d48d8334e1d5" class="cf7sr-g-recaptcha" data-sitekey="6LcpNmYUAAAAAE1MAxwYKMZTftm_Phevh7Ug7N2B">
+                                            <div style="width: 304px; height: 78px;"><div><iframe src="https://www.google.com/recaptcha/api2/anchor?ar=1&amp;k=6LcpNmYUAAAAAE1MAxwYKMZTftm_Phevh7Ug7N2B&amp;co=aHR0cHM6Ly93d3cuaXRndmlldG5hbS5jb206NDQz&amp;hl=en&amp;v=v1563777128698&amp;size=normal&amp;cb=ozej5hlsgrhy" width="304" height="78" role="presentation" name="a-7xo42y6vjug2" frameborder="0" scrolling="no" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-top-navigation allow-modals allow-popups-to-escape-sandbox"></iframe></div><textarea id="g-recaptcha-response" name="g-recaptcha-response" class="g-recaptcha-response" style="width: 250px; height: 40px; border: 1px solid rgb(193, 193, 193); margin: 10px 25px; padding: 0px; resize: none; display: none;"></textarea></div></div><span class="wpcf7-form-control-wrap cf7sr-g-recaptcha-invalid"></span><p></p>
+                                    <p>
+                                        <input type="submit" value="Gửi đi" class="wpcf7-form-control wpcf7-submit">
+                                        <span class="ajax-loader"></span></p>
+                                    <div class="wpcf7-response-output wpcf7-display-none">
+                                        </div>
+                                    </form>
+                                    </div>                                    
+                                    </div>
+            </div>
+            
+            
+        </div>
+        
+    </div>
+    <div class="container" style="padding: 30px 0;">
+            <div class="sup_box">
+                            <div class="row">
+                                <div class="col-md-4">
+                        <div class="thumb_sup">
+                            <img src="https://www.itgvietnam.com/wp-content/uploads/2018/06/tel.png" alt="">
+                        </div>
+                        <div class="ct_sup">
+                            <p style=" color: #fff;font-size: 14px">Hotline tư vấn giải pháp</p>
+                            <a href="tel:0986.196.838"style=" color: #fff;font-size: 14px"><p>0986.196.838</p></a>
+                        </div>
+                    </div>
+                                <div class="col-md-4">
+                        <div class="thumb_sup">
+                            <img src="https://www.itgvietnam.com/wp-content/uploads/2018/06/sp.png" alt="">
+                        </div>
+                        <div class="ct_sup">
+                            <p style=" color: #fff;font-size: 14px">Hỗ trợ kĩ thuật</p>
+                            <a href="tel:0966.966.051" style=" color: #fff;font-size: 14px"><p>0966.966.051</p></a>
+                        </div>
+                    </div>
+                                <div class="col-md-4">
+                        <div class="thumb_sup">
+                            <img src="https://www.itgvietnam.com/wp-content/uploads/2018/06/email.png" alt="">
+                        </div>
+                        <div class="ct_sup">
+                            <p style=" color: #fff;font-size: 14px">Gửi yêu cầu cho chúng tôi</p>
+                            <a href="tel:itg@3serp.vn" style=" color: #fff;font-size: 14px"><p>itg@3serp.vn</p></a>
+                        </div>
+                    </div>
+                            </div>
+            </div>
+        </div>
+</div>
+</body>
+</html>
