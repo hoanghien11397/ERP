387d6ebe3821ff2fc5a36cde262e3c08701e4dc4
hanhdoan1999 <doanhonghanh.1999@gmail.com>
2019-08-06 12:06:16 +0700

    ...

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,3 +1,62 @@
-<div class="jumbotron">
-<h1>footer</h1>
+    <!-- ***** Footer Area Start ***** -->
+    <footer class="footer-area clearfix">
+    <div class="container">
+    <div class="row">
+        <div class="col-md-3">
+            <div class="single-footer" style="border:none;">
+                <h5>VỀ ITG</h5>
+                <p>Tại ITG, chúng tôi luôn phấn đấu giúp các doanh nghiệp định hình lại phương pháp quản trị doanh nghiệp để vận hành tốt hơn, nâng cao hiệu quả kinh doanh và cải thiện đời sống con người, thông qua việc kết nối con người và công nghệ trong thời gian thực.</p>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="single-footer">
+                <h5>GIẢI PHÁP CHUYÊN NGÀNH</h5>
+                <ul>
+                    <li>Giải pháp ERP</li>
+                    <li>Quản trị doanh nghiệp</li>
+                    <li>Quản lý quan hệ khách hàng (CRM)</li>
+                    <li>Quản trị nguồn nhân lực (HRM)</li>
+                    <li>Doanh nghiệp thông minh (BI)</li>
+                    <li>Công nghệ mới</li>
+                </ul>
+            </div>    
+        </div>
+        <div class="col-md-3">
+            <div class="single-footer">
+                <h5>VP HÀ NỘI</h5>
+                <ul>
+                    <li>Tầng 7, Tòa nhà Báo Nông thôn Ngày nay, Lô E2 Cầu Giấy, Đường Dương Đình Nghệ, Cầu Giấy, Hà Nội</li>
+                    <li>Tel/Fax : 84 - 24 - 38339966</li>
+                    <li>Email : itg@3serp.vn</li>
+                    <li>Hotline tư vấn giải pháp : 0986.196.838</li>
+                    <li>Hotline hỗ trợ kỹ thuật : 0966.966.051/052/053</li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="single-footer">
+                <h5>VP HỒ CHÍ MINH</h5>
+                <ul>
+                    <li>159 Điện Biên Phủ, Phòng 104, Lầu 1, Phường 15, Quận Bình Thạnh, Tp. HCM</li>
+                    <li>Hotline tư vấn giải pháp : 092.6886.855</li>
+                    <li>Hotline hỗ trợ kỹ thuật : 092.6886.955</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <hr>
+    <div class="text-center">
+        <a href="">VỀ ITG</a>
+        <a href="">SẢN PHẨM</a>
+        <a href="">DỊCH VỤ</a>
+        <a href="">TIN TỨC</a>
+        <a href="">KHÁCH HÀNG</a>
+        <a href="">TUYỂN DỤNG</a>
+        <a href="">CHUYÊN ĐỀ</a>
+        <a href="">LIÊN HỆ</a>
+    </div>
+    <p class="text-center" style="padding:25px 0;color:#fff;">Copyright © ITG 2006 - 2019</p>
 </div>
+
+    </footer>
+    <!-- ***** Footer Area End ***** -->
