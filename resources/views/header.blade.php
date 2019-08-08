b1126a37e44fbe107fb3423574558fb0e91980ab
hanhdoan1999 <doanhonghanh.1999@gmail.com>
2019-08-08 19:24:00 +0700

    ...

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -8,55 +8,51 @@
                 <div class="col-12 h-100">
                     <div class="menu_area h-100">
                         <nav class="navbar h-100 navbar-expand-lg align-items-center">
-                            <!-- Logo -->
-                            <a class="navbar-brand" href=""><img src="https://www.itgvietnam.com/wp-content/uploads/2018/12/logo-4.svg" alt="logo" width="50%"></a>
-
-                            <!-- Menu Area -->
-                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mosh-navbar" aria-controls="mosh-navbar" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-
-                            <div class="collapse navbar-collapse" id="mosh-navbar">
+                            <div class="collapse navbar-collapse" id="moshnavbar">
+                                <!-- Logo -->
+                            <a class="navbar-brand" href=""><img src="./img/logo.JPEG" alt="logo" width="150px" height="60px"></a>
                                 <ul class="navbar-nav animated" id="nav">
                                     <li class="nav-item active"><a class="nav-link" href=""><i class="fas fa-home"></i> Trang chủ</a></li>
                                     <li class="nav-item"><a class="nav-link" href="{{ route('gioi_thieu') }}"><i class="fas fa-globe-asia"></i> Về ITG</a></li>
                                     <li class="nav-item dropdown">
                                         <a class="nav-link dropdown-toggle" href="{{ route('san_pham') }}" ><i class="far fa-question-circle"></i> Sản Phẩm</a>
-                                        <div class="dropdown-menu">
-                                            <a class="dropdown-item" href="">3S ERP 10i</a>
-                                            <a class="dropdown-item" href="">3S HRM</a>
-                                            <a class="dropdown-item" href="">3S CRM</a>  
-                                        </div>
+                                            <ul class="dropdown-menu">
+                                                <li><a class="dropdown-item" href="">3S ERP 10i</a></li>
+                                                <li><a class="dropdown-item" href="">3S HRM</a></li>
+                                                <li><a class="dropdown-item" href="">3S CRM</a></li> 
+                                            </ul>       
                                     </li>
                                     <li class="nav-item"><a class="nav-link" href="{{ route('dich_vu') }}"><i class="fas fa-cog"></i> Dịch vụ</a></li>
                                     <li class="nav-item dropdown">
                                         <a class="nav-link dropdown-toggle" href="{{ route('tin_tuc') }}" ><i class="far fa-newspaper"></i> Tin tức</a>
-                                        <div class="dropdown-menu">
-                                            <a class="dropdown-item" href="">Tin tức sự kiện</a>
-                                            <a class="dropdown-item" href="">Báo chí nói về chúng tôi</a> 
-                                        </div>
+                                        <ul class="dropdown-menu">
+                                            <li><a class="dropdown-item" href="">Tin tức sự kiện</a></li>
+                                            <a class="dropdown-item" href="">Báo chí nói về chúng tôi</a> </li>
+                                        </ul>
                                     </li>
                                     <li class="nav-item"><a class="nav-link" href="{{ route('khach_hang') }}"><i class="fas fa-users"></i> Khách hàng</a></li>
                                     <li class="nav-item"><a class="nav-link" href="{{ route('tuyen_dung') }}"><i class="far fa-newspaper"></i> Tuyển dụng</a></li>
                                     <li class="nav-item dropdown">
                                         <a class="nav-link dropdown-toggle" href="{{ route('chuyen_de') }}" ><i class="fas fa-list-ul"></i> Chuyên đề</a>
-                                        <div class="dropdown-menu">
-                                            <a class="dropdown-item" href="">ERP</a>
-                                            <a class="dropdown-item" href="">Phần mềm kế toán</a> 
-                                            <a class="dropdown-item" href="">CRM</a>
-                                            <a class="dropdown-item" href="">HRM</a> 
-                                            <a class="dropdown-item" href="">BI</a>
-                                            <a class="dropdown-item" href="">SCM</a> 
-                                            <a class="dropdown-item" href="">Quản trị doanh nghiệp</a>
-                                            <a class="dropdown-item" href="">Kế toán, kiểm toán</a> 
-                                        </div>
+                                        <ul class="dropdown-menu">
+                                            <li><a class="dropdown-item" href="">ERP</a></li>
+                                            <li><a class="dropdown-item" href="">Phần mềm kế toán</a></li> 
+                                            <li><a class="dropdown-item" href="">CRM</a></li>
+                                            <li><a class="dropdown-item" href="">HRM</a></li>
+                                            <li><a class="dropdown-item" href="">BI</a></li>
+                                            <li><a class="dropdown-item" href="">SCM</a></li>
+                                            <li><a class="dropdown-item" href="">Quản trị doanh nghiệp</a></li>
+                                            <li><a class="dropdown-item" href="">Kế toán, kiểm toán</a></li>
+                                        </ul>
                                     </li>
                                     <li class="nav-item"><a class="nav-link" href="{{ route('lien_he') }}"><i class="fas fa-map-marker-alt"></i> Liên hệ</a></li>
                                 </ul>
                                
-                                <div  style="margin-left:17px;">
+                                <div  style="margin-left:17px;" class="hid">
                                     <a href="#"><i class="fas fa-search"></i></a>
                                 </div> 
                                 <!-- Language -->
-                                <div style="margin-left:43px">
+                                <div style="margin-left:43px" class="hid">
                                     <a href="#"><img src="https://www.itgvietnam.com/wp-content/plugins/qtranslate-x/flags/vn.png" alt=""></a>
                                     <a href="#"><img src="https://www.itgvietnam.com/wp-content/plugins/qtranslate-x/flags/gb.png" alt=""></a>
                                 </div>
--- resources/views/pages/lien_he.blade.php
+++ resources/views/pages/lien_he.blade.php
@@ -6,100 +6,86 @@
 
 <!-- ****lien he **** -->
 <main id="lienhe">
-<div class="tp-bgimg defaultimg " data-bgcolor="undefined" style="background-repeat: no-repeat; background-image: url(&quot;https://www.itgvietnam.com/wp-content/uploads/2018/06/lien-he.jpg&quot;); background-size: cover; background-position: center center; width: 100%; height: 100%; opacity: 1; visibility: inherit; z-index: 20;"></div>
-<img  src="https://www.itgvietnam.com/wp-content/uploads/2018/06/lien-he.jpg">
-<div class="contact_box1">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6 add_col">
-                <ul class="ft_ct_list_lh">
-                    <li class="text-uppercase">CÔNG TY CỔ PHẦN GIẢI PHÁP ERP-ITG</li>
-                    <li>Văn phòng Hà Nội:</li>
-                    <li class="p_ct">Tầng 7, Tòa nhà Báo Nông thôn Ngày nay, Lô E2  Cầu Giấy, Đường Dương Đình Nghệ, Cầu Giấy, Hà Nội</li>
-                </ul>
-                <div class="g_maps">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.2956510297654!2d105.78384781485552!3d21.02085309342418!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab539595c3bd%3A0x39731e41b0eeb5d1!2zQ8O0bmcgdHkgQ1AgR2nhuqNpIFBow6FwIEVSUCAtIElURw!5e0!3m2!1svi!2s!4v1532686949267" width="550px" height="450" frameborder="0" style="border:0" allowfullscreen=""></iframe>                </div>
-                </div>
-            <div class="col-md-6">
-                <h3 class="col_title">Đăng ký tư vấn Giải pháp và nhận tài liệu miễn phí</h3>
-                <div class="ctf7_box">
+    <section class="bg-img">
+        <img src="https://www.itgvietnam.com/wp-content/uploads/2018/06/lien-he.jpg" alt="">  
+    </section>
 
-                    <div role="form" class="wpcf7" id="wpcf7-f2141-o1" lang="vi" dir="ltr">
-                        <div class="screen-reader-response"></div>
-                            <form action="/lien-he/#wpcf7-f2141-o1" method="post" class="wpcf7-form" novalidate="novalidate">
-                                <div style="display: none;">
-                                    <input type="hidden" name="_wpcf7" value="2141">
-                                    <input type="hidden" name="_wpcf7_version" value="5.1.3">
-                                    <input type="hidden" name="_wpcf7_locale" value="vi">
-                                    <input type="hidden" name="_wpcf7_unit_tag" value="wpcf7-f2141-o1">
-                                    <input type="hidden" name="_wpcf7_container_post" value="0">
-                                </div>
-                      
-                                    <p>
-                                        <span class="wpcf7-form-control-wrap your-name" style="">
-                                            <input type="text" name="your-name" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required form-control" aria-required="true" aria-invalid="false" placeholder="Họ tên" style="font-size: 14px"></span> </p>
-                                    <p>
-                                        <span class="wpcf7-form-control-wrap your-subject">
-                                            <input type="text" name="your-subject" value="" size="40" class="wpcf7-form-control wpcf7-text form-control" aria-invalid="false" placeholder="Tên doanh nghiệp tổ chức"style="font-size: 14px"></span> </p>
-                                    <p>
-                                        <span class="wpcf7-form-control-wrap your-tel">
-                                            <input type="tel" name="your-tel" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-tel wpcf7-validates-as-required wpcf7-validates-as-tel form-control" aria-required="true" aria-invalid="false" placeholder="Số điện thoại"style="font-size: 14px"></span> </p>
-                                    <p>
-                                        <span class="wpcf7-form-control-wrap your-email">
-                                            <input type="email" name="your-email" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email form-control" aria-required="true" aria-invalid="false" placeholder="Email"style="font-size: 14px"></span> </p>
-                                    <p>
-                                        <div class="form-group">
-                                            <textarea class="form-control" rows="5" id="comment" placeholder="Nội dung"></textarea>
-                                        </div>
-                                    <p>
-                                        </p>
-                                        <div id="cf7sr-5d48d8334e1d5" class="cf7sr-g-recaptcha" data-sitekey="6LcpNmYUAAAAAE1MAxwYKMZTftm_Phevh7Ug7N2B">
-                                            <div style="width: 304px; height: 78px;"><div><iframe src="https://www.google.com/recaptcha/api2/anchor?ar=1&amp;k=6LcpNmYUAAAAAE1MAxwYKMZTftm_Phevh7Ug7N2B&amp;co=aHR0cHM6Ly93d3cuaXRndmlldG5hbS5jb206NDQz&amp;hl=en&amp;v=v1563777128698&amp;size=normal&amp;cb=ozej5hlsgrhy" width="304" height="78" role="presentation" name="a-7xo42y6vjug2" frameborder="0" scrolling="no" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-top-navigation allow-modals allow-popups-to-escape-sandbox"></iframe></div><textarea id="g-recaptcha-response" name="g-recaptcha-response" class="g-recaptcha-response" style="width: 250px; height: 40px; border: 1px solid rgb(193, 193, 193); margin: 10px 25px; padding: 0px; resize: none; display: none;"></textarea></div></div><span class="wpcf7-form-control-wrap cf7sr-g-recaptcha-invalid"></span><p></p>
-                                    <p>
-                                        <input type="submit" value="Gửi đi" class="wpcf7-form-control wpcf7-submit">
-                                        <span class="ajax-loader"></span></p>
-                                    <div class="wpcf7-response-output wpcf7-display-none">
-                                        </div>
-                                    </form>
-                                    </div>                                    
+    <div class="contact_box1">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 ">
+                    <ul class="ft_ct_list_lh">
+                        <li class="text-uppercase">CÔNG TY CỔ PHẦN GIẢI PHÁP ERP-ITG</li>
+                        <li>Văn phòng Hà Nội:</li>
+                        <li class="p_ct">Tầng 7, Tòa nhà Báo Nông thôn Ngày nay, Lô E2  Cầu Giấy, Đường Dương Đình Nghệ, Cầu Giấy, Hà Nội</li>
+                    </ul>
+                    <div class="g_maps">
+                        <iframe width="100%" height="450px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.2956510297654!2d105.78384781485552!3d21.02085309342418!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab539595c3bd%3A0x39731e41b0eeb5d1!2zQ8O0bmcgdHkgQ1AgR2nhuqNpIFBow6FwIEVSUCAtIElURw!5e0!3m2!1svi!2s!4v1532686949267"></iframe>
+                       
+                    </div>
+                </div>    
+                <div class="col-md-6">
+                    <h3 class="col_title">Đăng ký tư vấn Giải pháp và nhận tài liệu miễn phí</h3>
+                    <form action="/action_page.php">
+                        <div class="form-group">
+                            <input type="text" class="form-control" id="name" placeholder="Họ tên">
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Tên doanh nghiệp tổ chức">
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Số điện thoại">
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Email">
+                        </div>
+                        <div class="form-group">
+                            <textarea class="form-control" rows="5" id="comment" placeholder="Nội dung"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Gửi đi</button>
+                    </form>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+    <div class="contact_box2">
+        <div class="container" style="padding: 30px 0;">
+                <div class="sup_box">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="thumb_sup">
+                                <img src="https://www.itgvietnam.com/wp-content/uploads/2018/06/tel.png" alt="">
+                            </div>
+                            <div class="ct_sup">
+                                <p style=" color: #fff;font-size: 14px">Hotline tư vấn giải pháp</p>
+                                <a href="tel:0986.196.838"style=" color: #fff;font-size: 14px"><p>0986.196.838</p></a>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="thumb_sup">
+                                <img src="https://www.itgvietnam.com/wp-content/uploads/2018/06/sp.png" alt="">
+                            </div>
+                            <div class="ct_sup">
+                                <p style=" color: #fff;font-size: 14px">Hỗ trợ kĩ thuật</p>
+                                <a href="tel:0966.966.051" style=" color: #fff;font-size: 14px"><p>0966.966.051</p></a>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="thumb_sup">
+                                <img src="https://www.itgvietnam.com/wp-content/uploads/2018/06/email.png" alt="">
+                            </div>
+                            <div class="ct_sup">
+                                <p style=" color: #fff;font-size: 14px">Gửi yêu cầu cho chúng tôi</p>
+                                <a href="tel:itg@3serp.vn" style=" color: #fff;font-size: 14px"><p>itg@3serp.vn</p></a>
+                            </div>
+                        </div>
+                </div>
                 </div>
             </div>
         </div>
-        
-    </div>
-    <div class="container" style="padding: 30px 0;">
-            <div class="sup_box">
-                            <div class="row">
-                                <div class="col-md-4">
-                        <div class="thumb_sup">
-                            <img src="https://www.itgvietnam.com/wp-content/uploads/2018/06/tel.png" alt="">
-                        </div>
-                        <div class="ct_sup">
-                            <p style=" color: #fff;font-size: 14px">Hotline tư vấn giải pháp</p>
-                            <a href="tel:0986.196.838"style=" color: #fff;font-size: 14px"><p>0986.196.838</p></a>
-                        </div>
-                    </div>
-                                <div class="col-md-4">
-                        <div class="thumb_sup">
-                            <img src="https://www.itgvietnam.com/wp-content/uploads/2018/06/sp.png" alt="">
-                        </div>
-                        <div class="ct_sup">
-                            <p style=" color: #fff;font-size: 14px">Hỗ trợ kĩ thuật</p>
-                            <a href="tel:0966.966.051" style=" color: #fff;font-size: 14px"><p>0966.966.051</p></a>
-                        </div>
-                    </div>
-                                <div class="col-md-4">
-                        <div class="thumb_sup">
-                            <img src="https://www.itgvietnam.com/wp-content/uploads/2018/06/email.png" alt="">
-                        </div>
-                        <div class="ct_sup">
-                            <p style=" color: #fff;font-size: 14px">Gửi yêu cầu cho chúng tôi</p>
-                            <a href="tel:itg@3serp.vn" style=" color: #fff;font-size: 14px"><p>itg@3serp.vn</p></a>
-                        </div>
-                    </div>
-                            </div>
-            </div>
-        </div>
-</div>
+    </div>    
+
 </main>
 @endsection
+
